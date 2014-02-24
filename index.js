72c565bf889666d6517eff88dbe44721f925f46e
Joseph Werle <joseph.werle@gmail.com>
2014-02-24 13:36:50 -0500

    add #parse and test cases

--- index.js
+++ index.js
@@ -1,23 +1,77 @@
 
-
 exports.stringify = function (obj) {
-
   function lines (obj) {
-    var l = []
+    var l = [];
     for(var k in obj) {
       if('object' === typeof obj[k])
         lines(obj[k]).forEach(function (e) {
-          l.push(k + '__' + e)
-        })
+          l.push(k + '__' + e);
+        });
       else
-        l.push(k + '="' + obj[k] + '"\n')
+        l.push(k + '="' + obj[k] + '"\n');
     }
-    return l
+    return l;
   }
 
-  return lines(obj).join('')
-}
+  return lines(obj).join('');
+};
 
-exports.parse = function (str) {
-  throw new Error('error: not implemented. please make pull-request')
-}
+exports.parse = function parse (str) {
+  var root = {};
+  var line = null;
+  var lines = str.split('\n');
+
+  function tick () {
+    return lines.shift();
+  }
+
+  function walk (line, scope) {
+    var offset = 0;
+    var ch = null;
+    var buf = [];
+    var key = null;
+    var value = null;
+    var isScalar = true;
+
+    function unseen () { return line.substr(offset); }
+    function advance () { return line[offset++]; }
+    function peek () { return line[offset + 1]; }
+    function prev () { return line[offset - 2]; }
+    function join () { var b = buf.join(''); return buf = [], b; }
+
+    while (ch = advance()) {
+      switch (ch) {
+        case '"': break;
+        case '_':
+          if ('_' == prev()) {
+            isScalar = false;
+            key = join();
+            return walk(unseen(), scope[key] = scope[key] || {});
+          }
+          break;
+
+        case '=':
+          key = join();
+          break;
+
+        default:
+          buf.push(ch);
+          if (null != key && true == isScalar && null == peek()) {
+            value = join();
+            if ('true' == value || 'false' == value) {
+              value = Boolean(value);
+            } else if (!isNaN(Number(value))) {
+              value = Number(value);
+            }
+            scope[key] = value;
+          }
+      }
+    }
+  }
+
+  while (line = tick()) {
+    walk(line, root);
+  }
+
+  return root;
+};
--- test.js
+++ test.js
@@ -1,10 +1,61 @@
 
-var BASH = require('./')
+var BASH = require('./');
+var assert = require('assert');
 
-console.log(BASH.stringify({
+var line = null;
+var lines = null;
+var str = null;
+var parsed = null;
+var expected = null;
+var o = {
   foo: true, bar: 'whatever',
   baz: {
     foo: 'foofoo',
-    bar: 1234
+    bar: 1234,
+    a: {
+      foo: 'foo',
+      bar: 'bar',
+      b: {
+        foo: 'foo',
+        bar: 'bar',
+        c: { foo: 'foo'}
+      }
+    }
   }
-}))
+};
+
+str = BASH.stringify(o);
+
+expected = [
+  'foo="true"', // .foo
+  'bar="whatever"', // .bar
+  'baz__foo="foofoo"', //.baz.foo
+  'baz__bar="1234"', //.baz.bar
+  'baz__a__foo="foo"', // .baz.a.foo
+  'baz__a__bar="bar"', // .baz.a.bar
+  'baz__a__b__foo="foo"', // .baz.a.b.foo
+  'baz__a__b__bar="bar"', // .baz.a.b.foo
+  'baz__a__b__c__foo="foo"', // .baz.a.b.c.foo
+  'baz__a__b__c__bar="bar"', // .baz.a.b.c.foo
+];
+
+lines = str.split('\n');
+while (line = lines.shift()) {
+  assert(expected.indexOf(line) > -1, line);
+}
+
+parsed = BASH.parse(str);
+assert(parsed);
+assert(Object.keys(parsed));
+
+assert(o.foo == parsed.foo);
+assert(o.bar == parsed.bar);
+assert(o.baz.foo == parsed.baz.foo);
+assert(o.baz.bar == parsed.baz.bar);
+assert(o.baz.a.foo == parsed.baz.a.foo);
+assert(o.baz.a.bar == parsed.baz.a.bar);
+assert(o.baz.a.b.foo == parsed.baz.a.b.foo);
+assert(o.baz.a.b.bar == parsed.baz.a.b.bar);
+assert(o.baz.a.b.c.foo == parsed.baz.a.b.c.foo);
+assert(o.baz.a.b.c.bar == parsed.baz.a.b.c.bar);
+
